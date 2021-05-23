9cf25c78360fccd2a063ae4e29411ef94f426a40
A.L <drellama72@gmail.com>
2021-05-23 15:14:42 -0400

    Reordered some of the code. Added a function to clear the drawing on the grid. Added checking for the input of the prompt to resize the grid.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,8 +1,6 @@
 const grid = document.querySelector('#grid');
-const resetButton = document.querySelector('#reset-button');
-let rows = 16;
-
-resetButton.addEventListener('click', resetGrid);
+const sizeButton = document.querySelector('#size-button');
+const clearButton = document.querySelector('#clear-button');
 
 function setGrid() {
   grid.style.gridTemplateColumns = `repeat(${rows}, 1fr)`;
@@ -19,11 +17,37 @@ function setGrid() {
 }
 
 function resetGrid() {
+  rows = parseInt(prompt('How many squares per side? [1-100]'));
+  
+  if (rows < 1 || rows > 100) {
+    alert('Please enter a number between 1 and 100');
+    return;
+  } else if (isNaN(rows)) {
+    return;
+  }
+
+  clearGrid();
+  setGrid();
+}
+
+function clearGrid() {
+  let gridSquares = document.querySelectorAll('.grid-square');
+
+  gridSquares.forEach((gridSquare) => {
+    gridSquare.remove();
+  })
+}
+
+function clearSketch() {
   let gridSquares = document.querySelectorAll('.grid-square');
 
   gridSquares.forEach((gridSquare) => {
     gridSquare.style.background = 'white';
   })
 }
+
+let rows = 16;
+sizeButton.addEventListener('click', resetGrid);
+clearButton.addEventListener('click', clearSketch);
 
 setGrid();
